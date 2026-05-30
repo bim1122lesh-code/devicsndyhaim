4d44c684adf64c1265c07a62fa7fdbb9b3709ed2
bim1122lesh-code <bim1122lesh@gmail.com>
2026-05-30 03:45:01 -0700

    devgfresd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4019894331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2540551579" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
